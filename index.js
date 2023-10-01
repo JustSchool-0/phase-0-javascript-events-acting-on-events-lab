384c7bb3336602cccd3a065d1cd0d6ff2223c8fb
Me <pdprince.lfc+github@gmail.com>
2023-09-30 20:35:50 -0400

    this hurts my soul

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+dodger.style.backgroundColor = "#DD00FF"
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if ((left + 40) < 400) {
+        dodger.style.left = `${left + 10}px`;
+    }
+}
